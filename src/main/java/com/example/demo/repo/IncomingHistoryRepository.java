cbebcb7367875c9eeb3d47418a53a41d735d9fc2
Eldar56048 <sayrambayeldar12@gmail.com>
2020-12-15 15:43:28 +0600

    First Release

--- src/main/java/com/example/demo/repo/IncomingHistoryRepository.java
+++ src/main/java/com/example/demo/repo/IncomingHistoryRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repo;
 
 import com.example.demo.models.IncomingHistory;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IncomingHistoryRepository extends CrudRepository<IncomingHistory,Long> {
